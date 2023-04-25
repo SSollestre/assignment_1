032b53370c84fda5b244f856174ff293ea4c249a
Sean Sollestre <sean.sollestre@gmail.com>
2023-04-25 11:36:56 -0700

    Add link to login from homepage

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.listen((3000), () => {
-    console.log('Server is running on port 3000; http://localhost:3000');
+app.listen((5000), () => {
+    console.log('Server is running on port 5000; http://localhost:5000');
 });
 
 app.get('/', (req, res) => {
-    res.send('Homepage');
+    res.send(`<h3>Homepage</h3>
+        <form action="./login/">
+            <input type="submit" value="login" />
+        </form>`)
 });
+
+app.get('/login', (req, res) => {
+    res.send(`<h3>Login</h3>
+<form>
+    <label for="username">Username</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Password</label><br>
+    <input type="text" id="password" name="password">
+</form>`)
+})
 
 const checkAuth = (req, res, next) => {
     console.log("Auth Route")

887205a5693cfe00df2d473683ccc4512bd17462
Sean Sollestre <sean.sollestre@gmail.com>
2023-04-25 11:21:29 -0700

    Add initial middleware for authenticated route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.listen((3000), () => {
+    console.log('Server is running on port 3000; http://localhost:3000');
+});
+
 app.get('/', (req, res) => {
     res.send('Homepage');
 });
 
-app.listen((3000), () => {
-    console.log('Server is running on port 3000');
-});
+const checkAuth = (req, res, next) => {
+    console.log("Auth Route")
+    next();
+};
+
+app.use(checkAuth)
+
+app.get('/authRoute', (req, res) => {
+    res.send('How did you get here?')
+})
